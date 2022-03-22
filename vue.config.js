28319f31f7a23d002bc83c65189f6f1ec900902c
yena <wyena924@gmail.com>
2022-03-22 16:32:17 +0900

    - vue.config.js에서 css loaderoption 추가 - app.vue 파일에 작성하였던 스타일 scss 파일로 분할

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,11 @@
 
 module.exports = {
+  css: {
+    loaderOptions: {
+      scss: {
+        additionalData: `@import "@/assets/css/main.scss";`
+      }
+    }
+  },
   transpileDependencies: true
 }
