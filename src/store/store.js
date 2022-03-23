fa441f7d6ce341dfcaa35f9fe446833b6695dcbf
yena <wyena924@gmail.com>
2022-03-23 15:12:10 +0900

    - vue 2에서 3으로 마이그레이션   - vue, @vue/compat 3.2.3   - vuex 4   - vue-cli 5, 플러그인 업그레이드   - vue-template-comiler 삭제 - 마이그레이션 가이드에 따라 main.js와 store.js 수정

--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,7 @@
 
-import Vue from 'vue'
-import Vuex from 'vuex'
- 
-Vue.use(Vuex);
- 
-export const store = new Vuex.Store({
+import { createStore } from 'vuex'
+
+export const store = createStore({
   state: {
     todoItems: JSON.parse(localStorage.getItem('todos-list-vue2')) || []
   },
